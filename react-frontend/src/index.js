5761d56ed478cfc1799fe405d452555ad213e43f
euberman <euberman@gmail.com>
2020-12-30 09:53:43 -0600

    got semantic ui to work properly

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './assets/index.css'
 
-//import './assets/semantic.min.css'
-
+import 'semantic-ui-css/semantic.min.css'
 
 import { Provider } from 'react-redux';
 import store from './store';
