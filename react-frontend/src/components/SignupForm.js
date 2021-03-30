2fda2bdfe0d873f8ad7629f0960759b6443a762e
ariel_grubbs <arielvictor1234@gmail.com>
2021-03-30 14:56:35 -0500

    messing around with sign in link on sign up form

--- react-frontend/src/components/SignupForm.js
+++ react-frontend/src/components/SignupForm.js
@@ -157,13 +157,13 @@ function SignupForm() {
                   Sign Up
                 </Button>
 
-                <Grid container justify="flex-end">
+                {/* <Grid container justify="flex-end">
                   <Grid item>
                     <Link href="http://localhost:3001/login" variant="body2">
                       Already have an account? Sign in
                     </Link>
                   </Grid>
-                </Grid>
+                </Grid> */}
             </form>
         </div>
     </Container>
